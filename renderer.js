9132f3fa10ef26e69b7ffdc4b6e3ca097bf6747c
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2016-05-21 11:33:00 -0300

    Just a hello world

--- renderer.js
+++ renderer.js
@@ -1,3 +1,30 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const AESjson = require('aes_json');
+
+var pencrypted = function(iv, salt, cipher){
+    encryptedout.value = AESjson.implode(iv, salt, cipher);
+    inputencrypted.value = encryptedout.value;
+};
+
+var pdecrypted = function(textBuffer){
+    dataout.value = new TextDecoder("utf-8").decode(textBuffer);
+};
+
+encryptbutton.onclick = function(){
+  var iv = AESjson.randchars(16), salt = AESjson.randchars(50);
+  
+  AESjson.importkey(password.value)
+  .then(AESjson.derivekey.bind(this, salt))
+  .then(AESjson.encrypt.bind(this, inputdata.value, iv))
+  .then(pencrypted.bind(this, iv, salt));
+};
+
+decryptbutton.onclick = function(){
+  var pack = AESjson.explode(inputencrypted.value),
+      salt = pack.salt,
+      iv = pack.iv;
+  AESjson.importkey(password.value)
+  .then(AESjson.derivekey.bind(this, salt))
+  .then(AESjson.decrypt.bind(this, pack.ciphertext, iv))
+  .then(pdecrypted);
+};
+
